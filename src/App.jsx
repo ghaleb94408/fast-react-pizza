9904f9a4cef470fb554c48523cf54db5d992a383
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-10 13:31:21 +0200

    add errorElement and navigation loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import {
-  BrowserRouter,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Cart from "./features/cart/Cart";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
-import AppLayout from "./ui/AppLayout";
+import Order from "./features/order/Order";
+import { loader as menuLoader } from "./features/menu/Menu";
 
 function App() {
   const router = createBrowserRouter([
     {
       element: <AppLayout />,
+      errorElement: <Error />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "/menu", element: <Menu /> },
+        {
+          path: "/menu",
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
         { path: "/cart", element: <Cart /> },
         { path: "/order/new", element: <CreateOrder /> },
         { path: "/order/:id", element: <Order /> },
