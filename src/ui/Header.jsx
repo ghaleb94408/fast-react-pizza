7bf11942dad8b60b418cf0e007c5be1ce8d1a121
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-12 15:04:17 +0200

    add initial responsive design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import UserName from "../features/user/UserName";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 export default function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
-      <Link to={"/"} className="tracking-wide">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to={'/'} className="tracking-wide">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
       <UserName />
     </header>
