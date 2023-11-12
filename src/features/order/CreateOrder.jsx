d855020ab6f4b87e03befa63ab10d6e439547456
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-13 00:10:23 +0200

    add buttons styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,30 +1,30 @@
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -35,7 +35,7 @@ function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
   return (
@@ -76,8 +76,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(fakeCart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order" : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="tracking-widetext-stone-800 inline-block rounded-full bg-yellow-400 px-4 py-3
+             font-semibold uppercase transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? 'Placing order' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -90,11 +94,11 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
   const errors = {};
   if (!isValidPhone(order.phone))
-    errors.phone = "Please provide a correct phone number";
+    errors.phone = 'Please provide a correct phone number';
   if (Object.keys(errors).length > 0) return errors;
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from '../features/order/SearchOrder';
 import UserName from '../features/user/UserName';
 export default function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to={'/'} className="tracking-wide">
         Fast React Pizza Co.
       </Link>
