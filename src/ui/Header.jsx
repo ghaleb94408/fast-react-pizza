c4ae0f3a319a89f5b7d599e6843c0ed95da80f04
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-12 13:47:29 +0200

    Add initial text and color styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 export default function Header() {
   return (
-    <header>
-      <Link to={"/"}>Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to={"/"} className="tracking-wide">Fast React Pizza Co.</Link>
       <SearchOrder />
-      <p>Jonas</p>
+      <UserName />
     </header>
   );
 }
