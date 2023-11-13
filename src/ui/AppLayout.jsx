27af49245dc607d6c67113f2a897ceef42bed8d5
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-13 12:52:23 +0200

    add loader styles

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,7 +9,7 @@ export default function AppLayout() {
     <div className="grid  h-screen  grid-rows-[auto_1fr_auto] ">
       {isLoading && <Loader />}
       <Header />
-      <div >
+      <div>
         <main className="mx-auto max-w-3xl ">
           <Outlet />
         </main>
